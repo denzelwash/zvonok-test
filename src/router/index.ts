1ab78987d7a58b401a31941468cdb19fda55c74a
Denis Obolevich <denzelwash33@yandex.ru>
2025-03-13 18:07:47 +0300

    Добавил 404 страницу

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import IndexView from '../views/IndexView.vue'
+import NotFoundView from '@/views/NotFoundView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,6 +9,11 @@ const router = createRouter({
       path: '/',
       name: 'index',
       component: IndexView,
+    },
+    {
+      path: '/:pathMatch(.*)*',
+      name: 'NotFound',
+      component: NotFoundView,
     },
   ],
 })
---
+++ src/views/NotFoundView.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+    <h1>404</h1>
+    <RouterLink to="/">На глвавную</RouterLink>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
