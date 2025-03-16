c61db82c7f923270f0351727a15b7a2e17bb183a
Denis Obolevich <denzelwash33@yandex.ru>
2025-03-16 16:26:20 +0300

    Добавил карту иконок

---
+++ src/const.ts
@@ -0,0 +1,22 @@
+const ICONS_MAP = {
+  '01d': 'icn_sun',
+  '01n': 'icn_sun',
+  '02d': 'icn_cloud',
+  '02n': 'icn_cloud',
+  '03d': 'icn_cloud',
+  '03n': 'icn_cloud',
+  '04d': 'icn_cloud',
+  '04n': 'icn_cloud',
+  '09d': 'icn_rain',
+  '09n': 'icn_rain',
+  '10d': 'icn_rain',
+  '10n': 'icn_rain',
+  '11d': 'icn_cloud',
+  '11n': 'icn_cloud',
+  '13d': 'icn_cloud',
+  '13n': 'icn_cloud',
+  '50d': 'icn_cloud',
+  '50n': 'icn_cloud',
+} as const
+
+export { ICONS_MAP }
