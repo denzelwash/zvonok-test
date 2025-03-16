19ce93ca375f20c7910f772f55fa7ab49e48e404
Denis Obolevich <denzelwash33@yandex.ru>
2025-03-16 22:07:01 +0300

    Стили для 404

--- src/pages/[...catchAll].vue
+++ src/pages/[...catchAll].vue
@@ -1,12 +1,29 @@
 <template>
   <div class="container">
     <div class="page-wrapper">
-      <div>
+      <div class="error">
         <h1>404</h1>
-        <RouterLink to="/">На глвавную</RouterLink>
+        <RouterLink to="/">На главную</RouterLink>
       </div>
     </div>
   </div>
 </template>
 
 <script setup lang="ts"></script>
+
+<style lang="scss" scoped>
+.page-wrapper {
+  justify-content: center;
+}
+.error {
+  text-align: center;
+  h1 {
+    margin-bottom: 8px;
+  }
+  a {
+    &:hover {
+      text-decoration: underline;
+    }
+  }
+}
+</style>
